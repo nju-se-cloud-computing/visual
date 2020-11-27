c6eb2a959f337454a5237d38e3a0c49aaeedd3ac
amanda <181250187@smail.nju.edu.cn>
2020-11-27 20:35:58 +0800

    feat: 实现从mongodb读取数据展示前端

--- cloud_visual.py
+++ cloud_visual.py
@@ -3,28 +3,31 @@ import csv
 import time
 
 from flask import Flask, render_template
-
+from pymongo import MongoClient
 from pyecharts import options as opts
 from pyecharts.charts import Bar, Pie, WordCloud
 
-# 获取bar的数据
-csv_path = "data/"
-modules_path = glob.glob("{}/*".format(csv_path))
-xaxis = []
-column = []
-for module in modules_path:
-    xaxis.append(module[5:])
+'''
+    从mongodb读取数据，将数据展现在前端
+
+'''
+
+uri = "mongodb+srv://Frank:123456789shi@cluster0.2hsme.gcp.mongodb.net/<Homework>?retryWrites=true&w=majority"
+dbname = 'Homework'
+readCol = 'visual_data'
 line_index = 0
+
+# 获取日期
 with open('data/一般零售/SH600280-1148334872-1138334820kline.csv') as f1:
     r = csv.reader(f1)
     time_column = [row[0] for row in r]
 
-# 获取world_cloud的数据
-with open('weighted_chg_percent.csv') as f2:
-    r2 = csv.reader(f2)
-    for row in r2:
-        modules = row[2:]
-        break
+# 存从数据库获取的数据
+data = {}
+
+'''
+    将时间戳转换成日期，yyyy-mm-dd
+'''
 
 
 def timeStamp(time_num):
@@ -32,49 +35,6 @@ def timeStamp(time_num):
     time_array = time.localtime(time_)
     other_style_time = time.strftime("%Y-%m-%d", time_array)
     return other_style_time
-
-
-# 更新bar的数据
-def update_ups_and_downs_bar(line_lidex):
-    yaxis = []
-    ups = []
-    downs = []
-    tmp = 0
-    for module in modules_path:
-        up = 0
-        down = 0
-        shares_path = glob.glob("{}/*.csv".format(module))
-        for share in shares_path:
-            with open(share) as f:
-                reader = csv.reader(f)
-                column = [row[6] for row in reader]
-                if len(column) < 285: continue
-            if column[line_lidex][0] == "-":
-                down += 1
-            else:
-                up += 1
-        ups.append(up)
-        downs.append(down)
-    yaxis.append(ups)
-    yaxis.append(downs)
-    return yaxis
-
-
-# 更新word_cloud 数据
-def update_word_cloud(line_index):
-    words = []
-    counter = 0
-    with open('weighted_chg_percent.csv') as f2:
-        r2 = csv.reader(f2)
-        for row in r2:
-            if counter == line_index:
-                value = row[2:]
-                break
-            else:
-                counter += 1
-    for i in range(1,len(modules)):
-        words.append((modules[i], float(value[i])))
-    return words
 
 
 app = Flask(__name__, static_folder="templates")
@@ -85,13 +45,53 @@ def get_date():
     return timeStamp(int(time_column[line_index]))
 
 
+''' 
+    读mongo
+    输入：字典。格式：{'module': '航空运输','time': '2019-08-09'}
+    返回：多个字典
+
+'''
+
+
+def readFromMongo(dic):
+    client = MongoClient(uri)
+    dblist = client.list_database_names()
+    if "Homework" in dblist:
+        print("数据库已找到！")
+    db = client[dbname]
+    collection = db[readCol]
+
+    doc = collection.find(dic)
+    array = []
+    for x in doc:
+        array.append(x)
+    return array[0]
+
+
+def docClean(doc, module, date):
+    max = -1
+    for item in doc:
+        if (str(item['data']) == date and item['module'] == module):
+            temp = int(str(item['_id'])[0:8], 16)
+            if (temp > max):
+                max = temp
+
+    for x in doc:
+        if int(str(x['_id'])[0:8], 16) == max and str(x['data']) == date and x['module'] == module:
+            print({'module': x['module'], 'time': x['data'], 'numerator': x['numerator']})
+            return {'module': x['module'], 'time': x['data'], 'numerator': x['numerator']}
+
+
 # 生产bar图像
-def bar_base(yaxis) -> Bar:
+def bar_base() -> Bar:
+    global data
+    data = readFromMongo({"date": get_date()})
+    print(data)
     c = (
         Bar()
-            .add_xaxis(xaxis)
-            .add_yaxis("上涨股票数", yaxis[0], stack="stack1")
-            .add_yaxis("下跌股票数", yaxis[1], stack="stack1")
+            .add_xaxis(data['modules'])
+            .add_yaxis("上涨股票数", data['ups'], stack="stack1")
+            .add_yaxis("下跌股票数", data['downs'], stack="stack1")
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
             .set_global_opts(
             title_opts=opts.TitleOpts(title=get_date() + "不同板块股票涨跌数柱状图"),
@@ -106,8 +106,8 @@ def bar_base(yaxis) -> Bar:
             .add(
             series_name="涨跌占比",
             data_pair=[
-                ["总上涨数", sum(yaxis[0])],
-                ["总下跌数", sum(yaxis[1])],
+                ["总上涨数", data['number_ups']],
+                ["总下跌数", data['number_downs']],
 
             ],
             center=["65%", "25%"],
@@ -115,16 +115,17 @@ def bar_base(yaxis) -> Bar:
         )
             .set_series_opts(tooltip_opts=opts.TooltipOpts(is_show=True, trigger="item"))
     )
+
     return c.overlap(pie)
 
 
 # 生成word_cloud图像
-def world_cloud(words) -> WordCloud:
+def world_cloud() -> WordCloud:
     c = (
         WordCloud()
             .add(
             "",
-            words,
+            data['word_cloud'],
             word_size_range=[20, 100],
             textstyle_opts=opts.TextStyleOpts(),
         )
@@ -143,18 +144,14 @@ def index():
 def get_bar_chart():
     global line_index
     line_index += 1
-    yaxis = update_ups_and_downs_bar(line_index)
-    print(yaxis)
-    c = bar_base(yaxis)
+    c = bar_base()
     return c.dump_options_with_quotes()
 
 
 # 获取word_cloud
 @app.route("/cloudChart")
 def get_cloud_chart():
-    words = update_word_cloud(line_index)
-    c = world_cloud(words)
-    print(words)
+    c = world_cloud()
     return c.dump_options_with_quotes()
 
 
