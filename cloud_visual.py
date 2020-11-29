78ad1f305dadb30fe097704cdefc150c763d54bb
amanda <181250187@smail.nju.edu.cn>
2020-11-29 15:27:26 +0800

    fix: 修改实时获取数据条形图

--- cloud_visual.py
+++ cloud_visual.py
@@ -16,7 +16,7 @@ from pyecharts.charts import Bar, Pie, WordCloud, Line
 uri = "mongodb+srv://Frank:123456789shi@cluster0.2hsme.gcp.mongodb.net/<Homework>?retryWrites=true&w=majority"
 dbname = 'Homework'
 readCol = 'visual_data'
-read_numerator = 'test4'
+read_numerator = 'test5'
 line_index = 0
 circul_all = {}
 
@@ -27,6 +27,7 @@ with open('data/一般零售/SH600280-1148334872-1138334820kline.csv') as f1:
 
 # 存从数据库获取的数据
 data = {}
+latestId = 0
 
 #
 
@@ -58,7 +59,7 @@ def get_date():
 '''
 
 
-def readFromMongo(dic,read):
+def readFromMongo(dic, read):
     client = MongoClient(uri)
     dblist = client.list_database_names()
     if "Homework" in dblist:
@@ -106,10 +107,13 @@ def get_circul_data():
         circul_all[module_name] = circul_sum
 
 
+get_circul_data()
+
+
 # 生产bar图像
 def bar_base() -> Bar:
     global data
-    data = readFromMongo({"date": get_date()},readCol)
+    data = readFromMongo({"date": get_date()}, readCol)
     print(data)
     c = (
         Bar()
@@ -157,12 +161,12 @@ def bar_base() -> Bar:
         Line()
             .add_xaxis(xaxis_data=data['modules'])
             .add_yaxis(
-            series_name="",
+            series_name="上涨股票占比率（百分数）",
             yaxis_index=1,
             y_axis=rate,
             label_opts=opts.LabelOpts(is_show=False),
             linestyle_opts=opts.LineStyleOpts(width=2),
-            is_smooth=True
+            is_smooth=True,
         )
     )
 
@@ -190,31 +194,80 @@ def world_cloud() -> WordCloud:
     return c
 
 
+def readLatest():
+    client = MongoClient(uri)
+    dblist = client.list_database_names()
+    if "Homework" in dblist:
+        print("数据库已找到！")
+    db = client[dbname]
+    collection = db[read_numerator]
+
+    doc = collection.find({'module': '半导体', 'data': '2019-08-09'})
+
+    docarray = []
+    for x in doc:
+        global latestId
+        dic = {}
+        id = int(str(x['_id'])[0:8], 16)
+        if id > latestId:
+            latestId = id
+            dic['module'] = x['module']
+            dic['date'] = x['data']
+            dic['numerator'] = x['numerator']
+            docarray.append(dic)
+    print(latestId)
+    return docarray
+
+
 def bar_ups_and_downs() -> Bar:
     # 这里要改成不断获取最新数据的函数
-    real_data = readFromMongo({"module": "半导体"},read_numerator)
-    print(real_data)
+    real_datas = readLatest()
+    print("real_datas:")
+    print(real_datas)
+    update_data = {}
 
-    up_and_down_data = round((float(real_data['numerator']) / circul_all[real_data['module']]), 2)
+    for i in range(len(circul_all)):
+        update_data[data['modules'][i]] = 0
+
+    for i in range(len(real_datas)):
+        real_data = real_datas[i]
+        up_and_down_data = round((float(real_data['numerator']) / circul_all[real_data['module']]), 2)
+        update_data[real_data['module']] = up_and_down_data
+
     bar_data = []
 
     for i in range(len(circul_all)):
-        if real_data['module'] == data['modules'][i]:
-            bar_data.append(up_and_down_data)
-        else:
+        if update_data[data['modules'][i]] == 0:
             bar_data.append(0)
-    c = (
-        Bar()
-            .add_xaxis(data['modules'])
-            .add_yaxis("实时获取的股票涨幅", bar_data)
-            .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-            .set_global_opts(
-            title_opts=opts.TitleOpts(title=real_data['date'] + " 实时获取板块股票涨跌幅柱状图"),
-            yaxis_opts=opts.AxisOpts(name="股票涨跌幅"),
-            xaxis_opts=opts.AxisOpts(name="板块名", is_show=True),
-            # datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
+        else:
+            bar_data.append(update_data[data['modules'][i]])
+    print(bar_data)
+    if real_datas == []:
+        c = (
+            Bar()
+                .add_xaxis(data['modules'])
+                .add_yaxis("实时获取的股票涨幅", bar_data)
+                .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+                .set_global_opts(
+                title_opts=opts.TitleOpts(title="实时获取板块股票涨跌幅柱状图"),
+                yaxis_opts=opts.AxisOpts(name="股票涨跌幅"),
+                xaxis_opts=opts.AxisOpts(name="板块名", is_show=True),
+                # datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
+            )
         )
-    )
+    else:
+        c = (
+            Bar()
+                .add_xaxis(data['modules'])
+                .add_yaxis("实时获取的股票涨幅", bar_data)
+                .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+                .set_global_opts(
+                title_opts=opts.TitleOpts(title=real_data['date'] + " 实时获取板块股票涨跌幅柱状图"),
+                yaxis_opts=opts.AxisOpts(name="股票涨跌幅"),
+                xaxis_opts=opts.AxisOpts(name="板块名", is_show=True),
+                # datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
+            )
+        )
     return c
 
 
@@ -247,5 +300,4 @@ def get_ups_and_downs_chart():
 
 
 if __name__ == "__main__":
-    get_circul_data()
     app.run()
